07eba35f1d0db40a810536a65f326bd50aabb3f0
ryan <whuk84@gmail.com>
2022-05-14 21:49:13 +0900

    first commit

---
+++ src/main/kotlin/me/ryan/black/hoody/HoodyApplication.kt
@@ -0,0 +1,11 @@
+package me.ryan.black.hoody
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class HoodyApplication
+
+fun main(args: Array<String>) {
+    runApplication<HoodyApplication>(*args)
+}
