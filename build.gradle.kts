c66c844ac606d1d0413e0f7a647adbe266f96364
ryan <whuk84@gmail.com>
2022-05-14 22:13:55 +0900

    Copy build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,9 +3,14 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 plugins {
     id("org.springframework.boot") version "2.6.7"
     id("io.spring.dependency-management") version "1.0.11.RELEASE"
+    id("org.jmailen.kotlinter") version "3.6.0"
+    id("com.avast.gradle.docker-compose") version "0.14.9"
     kotlin("jvm") version "1.6.21"
     kotlin("plugin.spring") version "1.6.21"
     kotlin("plugin.jpa") version "1.6.21"
+    kotlin("plugin.allopen") version "1.6.21"
+    kotlin("plugin.noarg") version "1.6.21"
+    kotlin("kapt") version "1.6.21"
 }
 
 group = "me.ryan.black"
@@ -13,29 +18,92 @@ version = "0.0.1-SNAPSHOT"
 java.sourceCompatibility = JavaVersion.VERSION_11
 
 repositories {
+    mavenLocal()
     mavenCentral()
 }
 
+ext["log4j2.version"] = "2.17.2"
+
 dependencies {
+    implementation("org.jetbrains.kotlin:kotlin-stdlib:1.6.21")
     implementation("org.springframework.boot:spring-boot-starter-actuator")
-    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
-    implementation("org.springframework.boot:spring-boot-starter-data-redis")
-    implementation("org.springframework.boot:spring-boot-starter-hateoas")
-    implementation("org.springframework.boot:spring-boot-starter-validation")
     implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+
+    implementation("org.springframework.boot:spring-boot-starter-data-redis")
+    implementation("org.springframework.boot:spring-boot-starter-validation")
+    implementation("org.springframework.boot:spring-boot-starter-undertow") {
+        exclude("io.undertow", "undertow-websockets-jsr")
+    }
+    testImplementation("org.springframework.boot:spring-boot-starter-test") {
+        exclude(group = "org.junit.vintage", module = "junit-vintage-engine")
+        exclude(group = "junit", module = "junit")
+    }
+    implementation("org.springframework.boot:spring-boot-starter-hateoas")
+    kapt("org.springframework.boot:spring-boot-configuration-processor")
+
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
-    implementation("org.jetbrains.kotlin:kotlin-reflect")
-    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
-    testImplementation("org.springframework.boot:spring-boot-starter-test")
+    implementation("com.fasterxml.jackson.datatype:jackson-datatype-jsr310")
+    implementation("com.fasterxml.jackson.datatype:jackson-datatype-jdk8")
+
+    kapt("com.querydsl:querydsl-apt:5.0.0:jpa")
+    implementation("com.querydsl:querydsl-core:5.0.0")
+    implementation("com.querydsl:querydsl-jpa:5.0.0")
+
+    testImplementation("io.kotest:kotest-runner-junit5-jvm:5.1.0")
+    implementation("org.springdoc:springdoc-openapi-ui:1.6.6")
+    implementation("org.springdoc:springdoc-openapi-kotlin:1.6.6")
+
+    testImplementation("com.h2database:h2")
+    runtimeOnly("mysql:mysql-connector-java:8.0.29")
+    testImplementation("de.flapdoodle.embed:de.flapdoodle.embed.mongo:3.3.1")
+    testImplementation("org.testcontainers:testcontainers:1.16.3")
+
+
+    implementation("org.apache.httpcomponents.client5:httpclient5:5.1.3")
+
 }
 
-tasks.withType<KotlinCompile> {
+configurations {
+    all {
+        exclude("org.springframework.boot", "spring-boot-starter-tomcat")
+    }
+}
+
+noArg {
+    invokeInitializers = true
+}
+
+allOpen {
+    annotation("javax.persistence.Entity")
+    annotation("javax.persistence.MappedSuperclass")
+    annotation("javax.persistence.Embeddable")
+}
+
+tasks.withType<KotlinCompile>().all {
     kotlinOptions {
         freeCompilerArgs = listOf("-Xjsr305=strict")
         jvmTarget = "11"
     }
 }
 
-tasks.withType<Test> {
+tasks.getByName<Test>("test") {
     useJUnitPlatform()
+
+    addTestListener(object : TestListener {
+        override fun beforeSuite(suite: TestDescriptor) {}
+        override fun beforeTest(testDescriptor: TestDescriptor) {}
+        override fun afterTest(testDescriptor: TestDescriptor, result: TestResult) {}
+        override fun afterSuite(suite: TestDescriptor, result: TestResult) {
+            if (suite.parent == null) {
+                val output =
+                    "Results: ${result.resultType} (${result.testCount} tests, ${result.successfulTestCount} successes, ${result.failedTestCount} failures, ${result.skippedTestCount} skipped)"
+                val startItem = "|  "
+                val endItem = "  |"
+                val repeatLength = startItem.length + output.length + endItem.length
+                println("\n${"-".repeat(repeatLength)}\n|  $output  |\n${"-".repeat(repeatLength)}")
+                println("\nElapsed: ${(result.endTime - result.startTime) / 1000} sec\n ")
+            }
+        }
+    })
 }
