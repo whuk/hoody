211d5f08424f549dded45ecaf9381bef40f15d18
ryan <whuk84@gmail.com>
2022-06-08 21:44:17 +0900

    Move password encoder to SecurityConfig.kt

--- src/main/kotlin/me/ryan/black/hoody/api/config/SecurityConfig.kt
+++ src/main/kotlin/me/ryan/black/hoody/api/config/SecurityConfig.kt
@@ -1,15 +1,17 @@
 package me.ryan.black.hoody.api.config
 
+import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.http.HttpMethod
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter
+import org.springframework.security.crypto.factory.PasswordEncoderFactories
+import org.springframework.security.crypto.password.PasswordEncoder
 
 @Configuration
 @EnableWebSecurity
 class SecurityConfig : WebSecurityConfigurerAdapter() {
-
     override fun configure(http: HttpSecurity) {
         http.csrf().disable()
         http.authorizeHttpRequests()
@@ -21,5 +23,8 @@ class SecurityConfig : WebSecurityConfigurerAdapter() {
         http.httpBasic()
     }
 
-
+    @Bean
+    fun passwordEncoder(): PasswordEncoder {
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder()
+    }
 }
